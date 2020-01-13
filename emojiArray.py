a7fe245a9412b85e300d6c80ff0292f0a6da7dde
Ethan Manarin <35640579+kindanon@users.noreply.github.com>
2020-01-13 14:48:09 -0500

    hurray for emoji library in python
    boo for programming emojis in general
    also emoji regex is hilariously 🅱️ig

--- emojiArray.py
+++ emojiArray.py
@@ -1,10 +1,18 @@
+import re
+
 emojis = open("winEmojisMin.txt","r+", encoding="utf-16").read()
 
 output = open("output.txt","w",encoding="utf-16")
 
 em = list(emojis)
-for x in em:
-    output.write("],["+x)
+#for x in em:
+#    output.write(x)
+
+lis = re.compile().split(emojis)
+
+
+
+output.write(lis[0])
 
 #output.write(list(emojis))
 #print(emojis.read())
